55202185529c1ab77859a0ad754cd40d1f9d3599
viettasc <51367503+viettasc@users.noreply.github.com>
2020-04-16 08:54:55 +0700

    Initial Commit

---
+++ Day82/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day82
+//
+//  Created by Viettasc Doan on 4/16/20.
+//  Copyright © 2020 Viettasc Doan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Day82Tests/Day82Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Day82Tests.swift
+//  Day82Tests
+//
+//  Created by Viettasc Doan on 4/16/20.
+//  Copyright © 2020 Viettasc Doan. All rights reserved.
+//
+
+import XCTest
+@testable import Day82
+
+class Day82Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
